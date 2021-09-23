a2ede0209ad7305fca9905f731306c9f1bd1407a
Manoj Kumar S D <manoj.kumarsd@rackspace.com>
2021-09-23 17:20:46 +0530

    updated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,5 @@
 provider "google" {
+  credentials = file("key.json")
   project = var.project_id
   region  = var.region
 }
@@ -9,9 +10,10 @@ provider "google-beta" {
 }
 
 terraform {
-  required_version = "0.14.7"
+  //required_version = "0.14.7"
 
   backend "gcs" {
     bucket = "1180845-tfstate"
     prefix = "caramel-spot-326810"
   }
+}
